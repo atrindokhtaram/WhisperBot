3090fe7924c0e0b811516f523f64985655c2b6ea
Bisnu Ray <79582318+bisnuray@users.noreply.github.com>
2024-10-06 18:49:15 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "123456"  # Replace this API ID with your actual API ID
+API_HASH = "12123456ABCDEFGHI  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "12345678:XXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+
+# Bot USER_NAME
+BOT_USERNAME = "LockTextBot" # Dont Use @ Just Put the bot username
