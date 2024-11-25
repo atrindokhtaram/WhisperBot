901604a0dcb245e604ff733d51ed0d67c03591d8
atrindokhtaram <moha.face9@gmail.com>
2024-11-25 03:41:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "12123456ABCDEFGHI  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "11057906"  # Replace this API ID with your actual API ID
+API_HASH = "b7f975dcdf30c826b3e6178ff3f72356"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7812155764:AAF49wqPvcYt0ctDvzk2elSVaxF0nw-rRBw"  # Replace this BOT_TOKEN
 
 # Bot USER_NAME
-BOT_USERNAME = "LockTextBot" # Dont Use @ Just Put the bot username
+BOT_USERNAME = "ACSSEPTOR_BOT" # Dont Use @ Just Put the bot username
